8b1711e5a69011545cb5a58bf54c69b59db17226
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2019-07-02 23:27:21 +0900

    코드 정렬

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) { 
+router.get('/login', function (req, res, next) {
   res.render('index');
 });
 
-router.post('/login', function(req,res){
+router.post('/login', function (req, res) {
   console.log(`post in`);
   console.log('login sucseece')
 });
