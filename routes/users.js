700fa7c192c7ec3be12f3b40e8629c54f57db3ad
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2019-07-09 22:49:39 +0900

    개선된 문법
    -로그인 오류 시 경고창 띄우기
    - 마이페이지 버튼 넘어가기
    - 로그아웃

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,25 @@ var express = require('express');
     
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express',session: req.session });
-});
-
-router.get('/login', function (req, res, next) {
-  res.render('index', { title: 'index', session: req.session.data });
-  
+  res.render('index', { title: 'index', session: req.session.data, message });
 });
 
 router.get('/home', function (req, res, next) {
   res.render('home', { title: 'home',session: req.session.data });
 });
 
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  console.log("session destroy 성공");
+  console.log("logout");
+  res.redirect('/');
+});
+
+router.get('/mypage', function (req, res, next) {
+  console.log(`mypage button sucess`);
+  res.render('change_pw', { title: 'mypage',session: req.session });
+});
+var message;
 router.post('/login', function (req, res) {
   console.log(`post in`);
   model.UserLogin(req.body.userName, req.body.userPassword, function (err, docs) {
@@ -30,9 +37,16 @@ router.post('/login', function (req, res) {
       res.redirect('/home');
     }
     else {
-      console.log('login error')
-      res.redirect('/login');
+      console.log('login error');
+      res.render('index', { title: 'index', message:1 });
     }
   });
 });
+
+router.post('/change_pw', function (req, res, next) {
+
+  console.log(`button sucess`);
+  res.redirect('/');
+});
+
 module.exports = router;
